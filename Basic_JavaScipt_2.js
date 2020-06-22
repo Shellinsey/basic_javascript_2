7681bd5937af5d95fdea35103115e363db98637b
Shellinsey <59896903+Shellinsey@users.noreply.github.com>
2020-06-21 19:21:16 -0700

    Add files via upload

---
+++ Basic_JavaScipt_2.js
@@ -0,0 +1,6 @@
+function javascript_function () {
+    var str= "I want this to be colored";
+    var result= str.fontcolor("green");
+    document.getElementById ("Green_text") .innerHTML = result;
+    
+}
